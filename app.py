97230e294eb134da633b8d669b07971ba49bf719
pedrodavid20 <pedrodvd012@gmail.com>
2025-01-31 07:55:17 -0300

    Niver juia

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("carregamento.html")
+
+@app.route("/PARABENS!!!!")
+def niver():
+    return render_template("niver.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
